db3297351c73bfeae2961a43b841e300a7d07dcd
Kevin Jennison <kevin.jennison1@gmail.com>
2019-03-29 10:07:59 -0700

    Remove unused code to open a tab after first install

--- src/shared/js/background.js
+++ src/shared/js/background.js
@@ -2,20 +2,6 @@
 
 import ext from './extension'
 
-// TODO: update URLs for these pages before enabling.
-
-// // On install, open a welcome tab.
-// ext.runtime.onInstalled.addListener(object => {
-//   try {
-//     if (object.reason === ext.runtime.OnInstalledReason.INSTALL) {
-//       const postInstallURL = 'https://tab.gladly.io/newtab/first-tab/'
-//       ext.tabs.create({ url: postInstallURL })
-//     }
-//   } catch (e) {
-//     console.error(e)
-//   }
-// })
-
 // On uninstall, open a post-uninstall page to get feedback.
 try {
   const postUninstallURL = 'https://tab.gladly.io/search/uninstalled/'
