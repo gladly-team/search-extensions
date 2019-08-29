4c347d005b1eee4e082b763dcb4fd0a8e7c46a86
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-29 15:22:46 -0700

    Open a welcome page on install

--- src/shared/js/background.js
+++ src/shared/js/background.js
@@ -2,6 +2,23 @@
 
 import ext from './extension'
 
+// On install, open a welcome tab.
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onInstalled
+try {
+  ext.runtime.onInstalled.addListener(details => {
+    try {
+      if (details.reason === ext.runtime.OnInstalledReason.INSTALL) {
+        const postInstallURL = 'https://tab.gladly.io/search/first-search/'
+        ext.tabs.create({ url: postInstallURL })
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  })
+} catch (e) {
+  console.error(e)
+}
+
 // On uninstall, open a post-uninstall page to get feedback.
 try {
   const postUninstallURL = 'https://tab.gladly.io/search/uninstalled/'
