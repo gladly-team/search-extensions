1c60dd000df02fe7e65db5e4eb9e44b4215edc11
Kevin Jennison <kevin.jennison1@gmail.com>
2022-08-02 14:37:48 -0400

    Modify build script to support separate builds for Chrome and Edge

--- scripts/build-chromium.js
+++ scripts/build-chromium.js
@@ -1,6 +1,5 @@
 /* eslint no-console: 0, import/no-extraneous-dependencies: 0, import/no-dynamic-require: 0 */
 
-const chromiumBrowsers = ['chrome', 'edge']
 const fs = require('fs-extra')
 const path = require('path')
 const { execSync } = require('child_process')
@@ -11,27 +10,47 @@ const archiver = require('archiver')
 // https://github.com/xpl/crx-hotreload/blob/master/hot-reload.js
 
 const BASE_BUILD_DIR = path.join(__dirname, '../build/')
-const BUILD_DIR = path.join(BASE_BUILD_DIR, 'chromium/')
 const SHARED_CODE_BUILD_DIR = path.join(
   __dirname,
   '../intermediate-builds/shared/'
 )
-const SRC_DIR = path.join(__dirname, '../src/chromium/')
-const manifest = require(path.join(SRC_DIR, 'manifest.json'))
-const { version } = manifest
 
-// Empty build target contents. This will also create the directory
-// if it does not exist.
-fs.emptyDirSync(BUILD_DIR)
+// Require the caller to pass the browser name.
+const CHROME_BROWSER = 'chrome'
+const EDGE_BROWSER = 'edge'
+const allowedBrowsers = [CHROME_BROWSER, EDGE_BROWSER]
+const scriptArgs = process.argv.slice(2)
+const buildTarget = scriptArgs[0]
+if (allowedBrowsers.indexOf(buildTarget) < 0) {
+  throw new Error('Specify either "chrome" or "edge" browser.')
+}
 
-chromiumBrowsers.forEach(browser => {
+const buildBrowser = browser => {
   process.env.BROWSER = browser
+  let buildDirectory
+  let sourceDirectory
+  if (browser === CHROME_BROWSER) {
+    buildDirectory = path.join(BASE_BUILD_DIR, 'chromium/')
+    sourceDirectory = path.join(__dirname, '../src/chromium/')
+  } else if (browser === EDGE_BROWSER) {
+    buildDirectory = path.join(BASE_BUILD_DIR, 'edge/')
+    sourceDirectory = path.join(__dirname, '../src/edge/')
+  } else {
+    throw new Error('Specify either "chrome" or "edge" browser.')
+  }
+
+  const manifest = require(path.join(sourceDirectory, 'manifest.json'))
+  const { version } = manifest
+
+  // Empty build target contents. This will also create the directory
+  // if it does not exist.
+  fs.emptyDirSync(buildDirectory)
 
   // Get the version number.
   console.log(`Building extension version ${version} for browser ${browser}...`)
 
   // Create the build version of the src.
-  const stageDir = path.join(BUILD_DIR, `${browser}-search-for-a-cause`)
+  const stageDir = path.join(buildDirectory, `${browser}-search-for-a-cause`)
 
   // Filter copying source files to build. Return true if we should copy and
   // false if we should not.
@@ -41,7 +60,7 @@ chromiumBrowsers.forEach(browser => {
     }
     return true
   }
-  fs.copySync(SRC_DIR, stageDir, { filter: filterCopiedFiles })
+  fs.copySync(sourceDirectory, stageDir, { filter: filterCopiedFiles })
   fs.removeSync(path.join(stageDir, '__tests__'))
 
   // Build the shared code and add it to the built extension.
@@ -50,7 +69,7 @@ chromiumBrowsers.forEach(browser => {
 
   // Create zip file.
   const zipFileName = `${browser}-search-for-a-cause-v${version}.zip`
-  const output = fs.createWriteStream(path.join(BUILD_DIR, zipFileName))
+  const output = fs.createWriteStream(path.join(buildDirectory, zipFileName))
   const archive = archiver('zip')
 
   // Listen for all archive data to be written.
@@ -72,4 +91,6 @@ chromiumBrowsers.forEach(browser => {
   // Finalize the archive (i.e. we are done appending files,
   // but streams still have to finish).
   archive.finalize()
-})
+}
+
+buildBrowser(buildTarget)
