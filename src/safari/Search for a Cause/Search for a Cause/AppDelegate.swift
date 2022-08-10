5fdaf0a7520adb5f91b90d13efccd26424d87ba1
Kevin Jennison <kevin.jennison1@gmail.com>
2022-08-10 10:47:06 -0400

    Set up default XCode project for Safari extension

---
+++ src/safari/Search for a Cause/Search for a Cause/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Search for a Cause
+//
+//  Created by Kevin Jennison on 8/10/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
